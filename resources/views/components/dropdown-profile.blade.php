7c93d61552fe041b8af3bd94eaa63bf3322b6428
AMC-3\LENOVO <coceijanina@yahoo.com>
2024-07-17 13:48:22 +0300

    User/Admin

--- resources/views/components/dropdown-profile.blade.php
+++ resources/views/components/dropdown-profile.blade.php
@@ -7,7 +7,7 @@
         class="inline-flex justify-center items-center group"
         aria-haspopup="true"
         @click.prevent="open = !open"
-        :aria-expanded="open"                        
+        :aria-expanded="open"
     >
         <img class="w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" width="32" height="32" alt="{{ Auth::user()->name }}" />
         <div class="flex items-center truncate">
@@ -18,7 +18,7 @@
         </div>
     </button>
     <div
-        class="origin-top-right z-10 absolute top-full min-w-44 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700/60 py-1.5 rounded-lg shadow-lg overflow-hidden mt-1 {{$align === 'right' ? 'right-0' : 'left-0'}}"                
+        class="origin-top-right z-10 absolute top-full min-w-44 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700/60 py-1.5 rounded-lg shadow-lg overflow-hidden mt-1 {{$align === 'right' ? 'right-0' : 'left-0'}}"
         @click.outside="open = false"
         @keydown.escape.window="open = false"
         x-show="open"
@@ -28,11 +28,17 @@
         x-transition:leave="transition ease-out duration-200"
         x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
-        x-cloak                    
+        x-cloak
     >
         <div class="pt-0.5 pb-2 px-3 mb-1 border-b border-gray-200 dark:border-gray-700/60">
             <div class="font-medium text-gray-800 dark:text-gray-100">{{ Auth::user()->name }}</div>
+            @if (Auth::user()->usertype == 'admin')
             <div class="text-xs text-gray-500 dark:text-gray-400 italic">Administrator</div>
+            @endif
+
+            @if (Auth::user()->usertype == 'user')
+            <div class="text-xs text-gray-500 dark:text-gray-400 italic">User</div>
+            @endif
         </div>
         <ul>
             <li>
@@ -50,8 +56,8 @@
                     >
                         {{ __('Sign Out') }}
                     </a>
-                </form>                                
+                </form>
             </li>
-        </ul>                
+        </ul>
     </div>
 </div>
